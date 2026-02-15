1f60d6d868aa55b55c3c1d0140f20e3ebbfc90f5
Mustafa Pakdil <mpakdil0@gmail.com>
2026-02-15 22:04:56 +0300

    fix: switch to bcryptjs and move all types to dependencies

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt, { SignOptions } from 'jsonwebtoken';
 import prisma, { isDatabaseAvailable } from '../config/database';
 import { config } from '../config/env';
